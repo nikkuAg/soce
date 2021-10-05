123ff7809a20ca80529375c97318854874b40362
Divyansh <a.divyansh.25@gmail.com>
2021-10-05 20:48:32 +0530

    Added comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,24 +34,66 @@ export const Home = () => {
                     </p>
                 </div>
             </div>
-            <div id="pragraph2">
-                <h3 id="individual">
-                    Unique features of the website:
-                </h3>
-                <ul>
-                    <li id="individual">Big compilation of previous opening and closing Ranks from year 2015 onwards</li>
-                    <li id="individual">Not a commercial website</li>
-                    <li id="individual">Does not ask about your personal details like Mobile Number, Roll number etc.</li>
-                    <li id="individual">Gives an idea about all branches and colleges on a single page</li>
-                    <li id="individual">Probability of getting a branch in a particular college is shown using different colour schemes for better understanding</li>
-                    <li id="individual">Prediction based on previous years’ opening as well as closing rank</li>
-                    <li id="individual">Also get the prediction by selecting an expected variation in previous years’ opening/ closing rank (in terms of %)</li>
-                    <li id="individual">For the sake of simplicity some minor modifications in the opening/ closing rank has been incorporated in the website. <span id="click" onClick={() => (setpopUp(true))} >Click</span> to get details. </li>
-                    <li id="individual">Developed by an IIT Roorkee student for the benefit of public</li>
-                    <li id="individual">New features can be added as per the requirement. Kindly send your feedback/ suggestions as per the link given on <a href="#/contact_us"><em>‘Contact Us’</em></a></li>
-                </ul>
-                <Popup show={popUp} close={setpopUp} />
+            <div id="content">
+                <div id="pragraph">
+                    <h3 id="individual">
+                        Unique features of the website:
+                    </h3>
+                    <ul>
+                        <li id="individual">Big compilation of previous opening and closing Ranks from year 2015 onwards</li>
+                        <li id="individual">Not a commercial website</li>
+                        <li id="individual">Does not ask about your personal details like Mobile Number, Roll number etc.</li>
+                        <li id="individual">Gives an idea about all branches and colleges on a single page</li>
+                        <li id="individual">Probability of getting a branch in a particular college is shown using different colour schemes for better understanding</li>
+                        <li id="individual">Prediction based on previous years’ opening as well as closing rank</li>
+                        <li id="individual">Also get the prediction by selecting an expected variation in previous years’ opening/ closing rank (in terms of %)</li>
+                        <li id="individual">For the sake of simplicity some minor modifications in the opening/ closing rank has been incorporated in the website. <span id="click" onClick={() => (setpopUp(true))} >Click</span> to get details. </li>
+                        <li id="individual">Developed by an IIT Roorkee student for the benefit of public</li>
+                        <li id="individual">New features can be added as per the requirement. Kindly send your feedback/ suggestions as per the link given on <a href="#/contact_us"><em>‘Contact Us’</em></a></li>
+                    </ul>
+                    <Popup show={popUp} close={setpopUp} />
+                </div>
+                <div id="updates">
+                    <h3>Thanks for your encouraging words</h3>
+                    <marquee direction="up" height="500" scrollamount="6" onMouseOver="this.stop()">
+                        <img src={require("../images/1.PNG").default} width="100%" />
+                        <img src={require("../images/2.PNG").default} width="100%" />
+                        <img src={require("../images/3.PNG").default} width="100%" />
+                        <img src={require("../images/4.PNG").default} width="100%" />
+                        <img src={require("../images/5.PNG").default} width="100%" />
+                        <img src={require("../images/6.PNG").default} width="100%" />
+                        <img src={require("../images/7.PNG").default} width="100%" />
+                        <img src={require("../images/8.PNG").default} width="100%" />
+                        <img src={require("../images/9.PNG").default} width="100%" />
+                        <img src={require("../images/10.PNG").default} width="100%" />
+                        <img src={require("../images/11.PNG").default} width="100%" />
+                        <img src={require("../images/12.PNG").default} width="100%" />
+                        <img src={require("../images/13.PNG").default} width="100%" />
+                        <img src={require("../images/14.PNG").default} width="100%" />
+                        <img src={require("../images/15.PNG").default} width="100%" />
+                        <img src={require("../images/16.PNG").default} width="100%" />
+                        <img src={require("../images/17.PNG").default} width="100%" />
+                        <img src={require("../images/18.PNG").default} width="100%" />
+                        <img src={require("../images/19.PNG").default} width="100%" />
+                        <img src={require("../images/20.PNG").default} width="100%" />
+                        <img src={require("../images/21.PNG").default} width="100%" />
+                        <img src={require("../images/22.PNG").default} width="100%" />
+                        <img src={require("../images/23.PNG").default} width="100%" />
+                        <img src={require("../images/24.PNG").default} width="100%" />
+                        <img src={require("../images/25.PNG").default} width="100%" />
+                        <img src={require("../images/26.PNG").default} width="100%" />
+                        <img src={require("../images/27.PNG").default} width="100%" />
+                        <img src={require("../images/28.PNG").default} width="100%" />
+                        <img src={require("../images/29.PNG").default} width="100%" />
+                        <img src={require("../images/30.PNG").default} width="100%" />
+                        <img src={require("../images/31.PNG").default} width="100%" />
+
+
+
+                    </marquee>
+                </div>
             </div>
+
             <div id="pragraph2">
                 <h3 id="individual2">
                     Disclaimer:
