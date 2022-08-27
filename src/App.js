09445541f96398bed4ccafcd3098f26c03b8427d
Divyansh Agarwal <a.divyansh.25@gmail.com>
2022-08-27 22:01:33 +0530

    Shift to new domain

--- src/App.js
+++ src/App.js
@@ -1,74 +1,17 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter, Route, HashRouter, Redirect } from 'react-router-dom';
-import { Home } from './components/Home';
-import { Colleges } from './components/Colleges';
-import { SeatMatrix } from './components/SeatMatrix';
-import { Ranks } from './components/Ranks';
-import { FormPrediction } from './components/FormPrediction';
-import { Prediction } from './components/Prediction';
-import { Contact } from './components/Contact';
-import { Use } from './components/Use';
-import { FormTrend } from './components/FormTrend';
-import { FormCTrend } from './components/FormCTrend';
-import { FormBTrend } from './components/FormBTrend';
-import { Choices } from './components/Choices';
-
-
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter, Route, HashRouter, Redirect } from "react-router-dom";
+import { Home } from "./components/Home";
 
 function App() {
-  let insSave = String(sessionStorage.getItem('ins'))
-  let poolSave = String(sessionStorage.getItem('pool'))
-  let categorySave = String(sessionStorage.getItem('category'))
-  let quotaSave = String(sessionStorage.getItem('quota'))
-  let rankSave = String(sessionStorage.getItem('rank'))
-  let yearSave = String(sessionStorage.getItem('year'))
-  let roundSave = String(sessionStorage.getItem('round'))
-  let optionSave = String(sessionStorage.getItem('option'))
-
-  const [ins] = useState(insSave === "null" ? "" : insSave)
-  const [seat_pool] = useState(poolSave === "null" ? "" : poolSave)
-  const [categoryValue] = useState(categorySave === "null" ? "" : categorySave)
-  const [quotaValue] = useState(quotaSave === "null" ? "" : quotaSave)
-  const [rank] = useState(rankSave === "null" ? "" : rankSave)
-  const [yearValue] = useState(yearSave === "null" ? "" : yearSave)
-  const [roundValue] = useState(roundSave === "null" ? "" : roundSave)
-  const [option] = useState(optionSave === "null" ? "" : optionSave)
-
-  const [institutes, setinstitute] = useState([])
-  const [branches, setbranch] = useState([])
-  const apiurl1 = "https://mysoce.pythonanywhere.com/soce/institutes/"
-  const apiurl2 = "https://mysoce.pythonanywhere.com/soce/branches/"
-  useEffect(() => {
-    axios.get(apiurl1)
-      .then(res => {
-        setinstitute(res.data)
-      })
-    axios.get(apiurl2)
-      .then(res => {
-        setbranch(res.data)
-      })
-  }, [])
-  return (
-    <BrowserRouter>
-      <HashRouter>
-        <Route path="/home" component={Home} />
-        <Route path="/colleges/:college" component={Colleges} />
-        <Route path="/seats/:college" component={() => (<SeatMatrix institutes={institutes} branches={branches} />)} />
-        <Route path="/ranks/:college" component={() => (<Ranks institutes={institutes} branches={branches} />)} />
-        <Route path="/prediction" component={() => (<FormPrediction />)} />
-        <Route path="/trend" component={() => (<FormTrend institutes={institutes} branches={branches} />)} />
-        <Route path="/branch_trend" component={() => (<FormCTrend institutes={institutes} branches={branches} />)} />
-        <Route path="/college_trend" component={() => (<FormBTrend institutes={institutes} branches={branches} />)} />
-        <Route path="/result" component={() => (<Prediction institutes={institutes} branches={branches} college={ins} pool={seat_pool} category={categoryValue} myRank={rank} quota={quotaValue} year={yearValue} round={roundValue} option={option} />)} />
-        <Route path="/contact_us" component={Contact} />
-        <Route path="/how_to_use" component={Use} />
-        <Route path="/choice" component={() => (<Choices institutes={institutes} branches={branches} />)} />
-        <Redirect to="/home" />
-      </HashRouter>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<HashRouter>
+				<Route path='/home' component={Home} />
+				<Redirect to='/home' />
+			</HashRouter>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,109 +1,24 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Icon, Label } from 'semantic-ui-react'
-import { Footer } from './Footer'
-import './home.css'
-import { MenuHeader } from './Menu'
-import { Popup } from './Popup'
-import { Timer } from './Timer'
+import React from "react";
+import "./home.css";
+import { MenuHeader } from "./Menu";
 
 export const Home = () => {
-    const [popUp, setpopUp] = useState(false)
-    const [update, setupdate] = useState([])
-    const [loading, setloading] = useState(true)
-    const api = "https://mysoce.pythonanywhere.com/soce/update/"
-    useEffect(() => {
-        axios.get(api)
-            .then(res => {
-                setupdate(res.data.reverse())
-                setloading(false)
-            })
-    }, [api])
-
-    return (
-        <React.Fragment>
-            <MenuHeader active='home' set={false} />
-            <h2 className="headingFull"><span className="headingFirst">G</span>o for <span className="headingFirst">S</span>election <span className="headingFirst">O</span>f <span className="headingFirst">C</span>olleges for <span className="headingFirst">E</span>ngineering <span className="fullform">(GoSOCE)</span></h2>
-            <div id="content" className="first">
-                <div id="myTimer">
-                    <div id="timer2">
-                        <Timer for="filling choice for JoSAA Mock Round - 2" year={2021} month={9} day={23} hour={17} min={0} sec={0} />
-                    </div>
-                    <div id="updates">
-                        <h3>Recent Updates</h3>
-                        {loading ? <></> :
-                            <marquee direction="up" height="250" scrollamount="2">
-                                <ul type="square" id="ulText">
-                                    {update.map(text => (
-                                        <li id="updateItem" key={text.id}>{text.text}</li>
-                                    ))}
-                                </ul>
-                            </marquee>
-                        }
-                    </div>
-                </div>
-                <div id="timeLeft">
-                    <div id="timer1">
-                        <Timer for="filling choice for JoSAA Mock Round - 2" year={2021} month={9} day={23} hour={17} min={0} sec={0} />
-                    </div>
-                </div>
-
-            </div>
-            <div id="increaseImg">
-                <p id="heading1">Summary of Seats Increased/Decreased in JoSAA 2021</p>
-                <p id="heading2">(For more details kindly visit 'Seat Matrix' {`->`} 'Seat Increased/Decreased in JoSAA 2021')</p>
-                <img src={require("../images/increase.png").default} width="100%" />
-            </div>
-            <div id="pragraphM">
-                <h3 id="individual">
-                    JEE (Mains) and JEE (Advance) Over…
-                </h3>
-                <p id="individual">Now it’s time to choose best college and branch according to your rank in the exams… but how?
-                </p>
-                <p id="individual"> Here is the solution…
-                </p>
-                <p id="individual"> This website has been designed to help the Engineering aspirants and their parents to know about
-                    the previous years’ cut-off Rank for various colleges and branches so that they can take an informed
-                    decision to choose best college and branch according to the rank secured in the exam.For better
-                    understanding click on <a href="#/how_to_use"><em>‘How to use’</em></a>
-                </p>
-            </div>
-            <div id="content" className="chnages2">
-                <div id="pragraph">
-                    <h3 id="individual">
-                        Unique features of the website:
-                    </h3>
-                    <ul>
-                        <li id="individual">Big compilation of previous opening and closing Ranks from year 2015 onwards</li>
-                        <li id="individual">Not a commercial website</li>
-                        <li id="individual">Does not ask about your personal details like Mobile Number, Roll number etc.</li>
-                        <li id="individual">Gives an idea about all branches and colleges on a single page</li>
-                        <li id="individual">Probability of getting a branch in a particular college is shown using different colour schemes for better understanding</li>
-                        <li id="individual">Prediction based on previous years’ opening as well as closing rank</li>
-                        <li id="individual">Also get the prediction by selecting an expected variation in previous years’ opening/ closing rank (in terms of %)</li>
-                        <li id="individual">For the sake of simplicity some minor modifications in the opening/ closing rank has been incorporated in the website. <span id="click" onClick={() => (setpopUp(true))} >Click</span> to get details. </li>
-                        <li id="individual">Developed by an IIT Roorkee student for the benefit of public</li>
-                        <li id="individual">New features can be added as per the requirement. Kindly send your feedback/ suggestions as per the link given on <a href="#/contact_us"><em>‘Contact Us’</em></a></li>
-                    </ul>
-                    <Popup show={popUp} close={setpopUp} />
-                </div>
-                <div id="updates" className="changes">
-                    <h3>New SOCE Trend</h3>
-                    <p id="trendD">for<br />IIT Kanpur (Electrical 4 years) (General, Gender Neutral).<br />JEE Adv Rank: 920</p>
-                    <img src={require("../images/trend.png").default} width="100%" />
-                    <p>More such trend refer to <a id="soceTrend" href="#/trend" >SOCE Trend</a></p>
-                </div>
-            </div>
-
-            <div id="pragraph2">
-                <h3 id="individual2">
-                    Disclaimer:
-                </h3>
-                <p id="individual2">
-                    This website uses various relevant data available in open domain. Though utmost care has been taken while preparing this website, however it is advised to refer official websites before taking any decision regarding selection of college and branch based on the information available on this website. We will not be responsible for any mistake and inconformity in the information available on this website.
-                </p>
-            </div>
-            <Footer />
-        </React.Fragment >
-    )
-}
+	return (
+		<React.Fragment>
+			<MenuHeader active='home' set={false} />
+			<h2 className='headingFull'>
+				<span className='headingFirst'>R</span>ank
+				<span className='headingFirst'>M</span>atrix
+			</h2>
+			<div id='content' className='first'>
+				<div id='myTimer'>
+					We have redesigned this webiste and shifted to a new domain. Please
+					refer{"  "}
+					<a href='https://rankmatrix.iitr.ac.in'>
+						https://rankmatrix.iitr.ac.in
+					</a>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
